3c3c4fe352b0a654d5478a0861ec0c6010b0ade8
Tal Kohavy <talkohavy@gmail.com>
2026-07-04 07:28:27 +0300

    small

--- src/global.d.ts
+++ src/global.d.ts
@@ -19,6 +19,7 @@ declare module '*.svg' {
 
   // 2. When config is set to: exportType: 'named':
   import type React from 'react';
+
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   // biome-ignore lint: ambient module type declaration
   export { ReactComponent };
